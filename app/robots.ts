986708845744adf7c960f6c88e631dfda6492499
ainibosystems-official <ainibosystems@gmail.com>
2026-01-20 17:20:33 +0200

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,17 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: ["/api/"],
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: [
+          "/api/",
+          "/_next/",
+          "/favicon.ico",
+        ],
+      },
+    ],
     sitemap: "https://www.nibofix.com/sitemap.xml",
   };
 }
